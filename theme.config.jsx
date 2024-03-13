4394c06f15383b567c413de8c6fd536f40889c2b
PRadeep Kumar <prithviraj.rudraksh@gmail.com>
2024-03-14 00:14:04 +0530

    Initial commit

---
+++ theme.config.jsx
@@ -0,0 +1,51 @@
+export default {
+  logo: <span>Crudsify Documentation</span>,
+  project: {
+    link: "https://github.com/prudradeep/crudsify",
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – Crudsify",
+    };
+  },
+  banner: {
+    key: "1.0-release",
+    text: (
+      <a href="https://nextra.site" target="_blank">
+        🎉 Nextra 1.0 is released. Read more →
+      </a>
+    ),
+  },
+  sidebar: {
+    autoCollapse: true,
+    defaultMenuCollapseLevel: 1,
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Crudsify" />
+      <meta
+        property="og:description"
+        content="CRUDSIFY - A Simplified RESTful API generator"
+      />
+    </>
+  ),
+  editLink: {
+    component: null,
+  },
+  feedback: {
+    content: "Question? Give us feedback",
+    useLink: () => "https://github.com/prudradeep/crudsify/issues/new",
+  },
+  footer: {
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{" "}
+        <a href="https://crudsify.com" target="_blank">
+          Crudsify
+        </a>
+        .
+      </span>
+    ),
+  },
+};
